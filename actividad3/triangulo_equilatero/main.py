7ae726a0df155e04eb13194b85f53e61e2fc05b0
jjaramza <jejaramillo@unal.edu.co>
2024-12-22 02:44:12 -0500

    Update EquilateroApp

--- actividad3/triangulo_equilatero/main.py
+++ actividad3/triangulo_equilatero/main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import messagebox
+import re
 from equilatero import Equilatero
 
 class EquilateroApp:
@@ -6,125 +8,116 @@ class EquilateroApp:
     def __init__(self, root: tk.Tk):
         self.root = root
         self.root.title("Triángulo Equilátero")
-        self.root.geometry("400x380")
+        self.root.geometry("350x200")
         self.root.resizable(False, False)
         
         # Frame principal
-        self.frame_principal = tk.Frame(master=self.root)
+        self.frame_principal = tk.Frame(master=self.root, padx=20, pady=20)
         self.frame_principal.pack(expand=True)
         
-        # Título frame
-        self.label_titulo = tk.Label(
-            master=self.frame_principal,
-            text="Triángulo Equilátero",
-            font=("Arial", 18, "bold"),
-            fg="blue"
-        )
-        self.label_titulo.grid(column=0, row=0, columnspan=2, pady=10)
+        # Variable para almacenar el valor de entrada
+        self.stringvar_lado = tk.StringVar()
         
-        # Nombre de etiquetas
-        self.label_lado = tk.Label(
-            master=self.frame_principal,
-            text="Lado (cm):",
-            font=("Arial", 14)
-        )
-        self.label_lado.grid(column=0, row=1, padx=10, pady=10, sticky=tk.W)
-        self.label_perimetro = tk.Label(
-            master=self.frame_principal,
-            text="Perímetro (cm):",
-            font=("Arial", 14)
-        )
-        self.label_perimetro.grid(column=0, row=2, padx=10, pady=10, sticky=tk.W)
-        self.label_altura = tk.Label(
-            master=self.frame_principal,
-            text="Altura (cm):",
-            font=("Arial", 14)
-        )
-        self.label_altura.grid(column=0, row=3, padx=10, pady=10, sticky=tk.W)
-        self.label_area = tk.Label(
-            master=self.frame_principal,
-            text="Área (cm²):",
-            font=("Arial", 14)
-        )
-        self.label_area.grid(column=0, row=4, padx=10, pady=10, sticky=tk.W)
-
-        # Campo de entrada
-        self.entry_lado = tk.Entry(
-            master=self.frame_principal,
-            font=("Arial", 14),
-            width=5
-        )
-        self.entry_lado.grid(column=1, row=1, padx=10, pady=10, sticky=tk.E)
+        # Validación en tiempo real
+        self.stringvar_lado.trace_add(mode="write", callback=self.validar_lado)
         
-        # Etiquetas de salida
-        self.label_perimetro_resultado = tk.Label(
-            master=self.frame_principal,
-            text="0.00",
-            font=("Arial", 14, "bold")
-        )
-        self.label_perimetro_resultado.grid(column=1, row=2, padx=10, pady=10, sticky=tk.E)
-        self.label_altura_resultado = tk.Label(
-            master=self.frame_principal,
-            text="0:00",
-            font=("Arial", 14, "bold")
-        )
-        self.label_altura_resultado.grid(column=1, row=3, padx=10, pady=10, sticky=tk.E)
-        self.label_area_resultado = tk.Label(
-            master=self.frame_principal,
-            text="0:00",
-            font=("Arial", 14, "bold")
-        )
-        self.label_area_resultado.grid(column=1, row=4, padx=10, pady=10, sticky=tk.E)
+        # Creación de los widgets
+        self.creacion_widgets()
         
-        # Botones
+        # Botón para calcular
         self.button_calcular = tk.Button(
             master=self.frame_principal,
             text="Calcular",
-            font=("Arial", 14),
-            command=self.calcular_propiedades
+            command=self.mostrar_resultados,
+            width=20,
+            state=tk.DISABLED
         )
-        self.button_calcular.grid(column=0, row=5, padx=10, pady=10, sticky=tk.W)
-        self.button_limpiar = tk.Button(
-            master=self.frame_principal,
-            text="Limpiar",
-            font=("Arial", 14),
-            command=self.limpiar_campos
-        )
-        self.button_limpiar.grid(column=1, row=5, padx=10, pady=10, sticky=tk.E)
+        self.button_calcular.pack(pady=10)
         
-        # Etiqueta de advertencias
-        self.label_advertencia = tk.Label(
+        # Estado validación del campo
+        self.campos_validos = {
+            "lado": False
+        }
+    
+    def creacion_widgets(self):
+        """Campos del formulario con sus etiquetas."""
+        # Campo lado de un triángulo equilátero
+        tk.Label(master=self.frame_principal, text="Lado (en cm):").pack(anchor=tk.W)
+        self.entry_lado = tk.Entry(
             master=self.frame_principal,
-            text="Ingrese solo números positivos.",
-            font=("Arial", 12, "bold")
+            textvariable=self.stringvar_lado,
+            width=30
         )
-        self.label_advertencia.grid(column=0, row=6, columnspan=2, pady=10)
+        self.entry_lado.focus()
+        self.entry_lado.pack()
+        self.label_error_lado = tk.Label(
+            master=self.frame_principal,
+            text="",
+            fg="red",
+            wraplength=300
+        )
+        self.label_error_lado.pack()
+    
+    def validar_lado(self, *args):
+        """Valida que el campo lado del triángulo contenga solo números."""
+        lado = self.stringvar_lado.get()
+        if not lado:
+            self.label_error_lado.config(text="Campo requerido.")
+            self.campos_validos["lado"] = False
+        elif not re.match(pattern=r"^[0-9]+(\.[0-9]+)?$", string=lado):
+            self.label_error_lado.config(text="Ingrese solo números.")
+            self.campos_validos["lado"] = False
+        else:
+            self.label_error_lado.config(text="")
+            self.campos_validos["lado"] = True
+        self.actualizar_boton_calcular()
+    
+    def actualizar_boton_calcular(self):
+        """Actualiza el estado del botón de acuerdo a la validación de los campos."""
+        if all(self.campos_validos.values()):
+            self.button_calcular.config(state=tk.NORMAL)
+        else:
+            self.button_calcular.config(state=tk.DISABLED)
     
     def calcular_propiedades(self):
-        lado = self.entry_lado.get().strip()
+        """Extraer la información del campo lado para calcular las propiedades."""
+        lado = float(self.stringvar_lado.get())
         
-        if lado == "":
-            self.label_advertencia.config(text="El campo no puede estar vacío.", fg="red")
-        elif not lado.replace(".", "").isdigit() or lado.count(".") > 1:
-            self.label_advertencia.config(text="Ingrese un número válido.", fg="red")
-        else:
-            equilatero = Equilatero(float(lado))
-            perimetro = equilatero.perimetro_equilatero()
-            altura = equilatero.altura_equilatero()
-            area = equilatero.area_equilatero()
-            self.label_perimetro_resultado.config(text=f"{perimetro:.2f}")
-            self.label_altura_resultado.config(text=f"{altura:.2f}")
-            self.label_area_resultado.config(text=f"{area:.2f}")
-            self.label_advertencia.config(text="Cálculos realizados con éxito.", fg="green")
+        equilatero = Equilatero(lado=lado)
+        
+        perimetro = equilatero.perimetro_equilatero()
+        altura = equilatero.altura_equilatero()
+        area = equilatero.area_equilatero()
+        
+        return {
+            "perimetro": perimetro,
+            "altura": altura,
+            "area": area
+        }
+    
+    def mostrar_resultados(self):
+        """Obtiene los datos para mostrar los resultados."""
+        propiedades = self.calcular_propiedades()
+        mensaje = f"""
+        Propiedades Triángulo Equilátero:
+        
+        Perímetro (cm):\t{propiedades["perimetro"]:.2f}
+        Altura (cm):\t\t{propiedades["altura"]:.2f}
+        Área (cm²):\t\t{propiedades["area"]:.2f}
+        """
+        messagebox.showinfo(title="Resultados", message=mensaje)
+        self.limpiar_campos()
     
     def limpiar_campos(self):
-        self.entry_lado.delete(first=0, last=tk.END)
-        self.label_perimetro_resultado.config(text="0.00")
-        self.label_altura_resultado.config(text="0.00")
-        self.label_area_resultado.config(text="0.00")
-        self.label_advertencia.config(text="Ingrese solo números positivos.", fg="black")
+        """Valores por defecto del framde"""
+        self.stringvar_lado.set(value="")
+        self.label_error_lado.config(text="")
+        self.entry_lado.focus()
+    
 
+def main():
+    root = tk.Tk()
+    EquilateroApp(root=root)
+    root.mainloop()
 
-root = tk.Tk()
-EquilateroApp(root=root)
-root.mainloop()
+main()
